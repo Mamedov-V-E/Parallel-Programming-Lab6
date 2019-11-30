bef7fea5eeebf7ef9e7513ecf8f94dd9def8f334
Mamedov-V-E <black_snake666@mail.ru>
2019-11-30 10:52:02 +0300

    Scripted auto-commit on change (2019-11-30 10:52:02) by gitwatch.sh

--- src/main/java/ConfigurationStorageActor.java
+++ src/main/java/ConfigurationStorageActor.java
@@ -1,2 +1,9 @@
-public class ConfigurationStorageActor {
+import akka.actor.AbstractActor;
+import akka.http.javadsl.server.Route;
+
+public class ConfigurationStorageActor extends AbstractActor {
+
+    public Route createRoute() {
+        return createRoute().
+    }
 }
