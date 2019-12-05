666d0b35c452fbfe9e412702529248d2d68f7a7e
Mamedov-V-E <black_snake666@mail.ru>
2019-12-05 22:48:02 +0300

    Scripted auto-commit on change (2019-12-05 22:48:02) by gitwatch.sh

--- src/main/java/FlowFactory.java
+++ src/main/java/FlowFactory.java
@@ -1,4 +1,14 @@
+import akka.NotUsed;
+import akka.actor.ActorSystem;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class FlowFactory {
 
-    public Flow<R>
+    public Flow<HttpRequest, HttpResponse, NotUsed> createFlow(ActorSystem system, ActorMaterializer materializer) {
+
+    }
 }
--- src/main/java/ZooKeeperApp.java
+++ src/main/java/ZooKeeperApp.java
@@ -27,6 +27,6 @@ public class ZooKeeperApp {
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
-                FlowFa
+                FlowFactory.
     }
 }
