5dc73bc5461ac7a50dce11387c26a70705de54d6
Mamedov-V-E <black_snake666@mail.ru>
2019-12-05 22:35:52 +0300

    Scripted auto-commit on change (2019-12-05 22:35:52) by gitwatch.sh

--- src/main/java/ZooKeeperApp.java
+++ src/main/java/ZooKeeperApp.java
@@ -1,7 +1,19 @@
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+
 public class ZooKeeperApp {
+    public static final String ACTOR_SYSTEM_NAME = "zooKeeperSystem";
+
     public static void main (String[] args) {
         if (args.length < 1) {
             System.out.println("Server port specified");
+            System.exit(-1);
         }
+
+        ActorSystem system = ActorSystem.create(ACTOR_SYSTEM_NAME);
+        ActorRef configActor = system.actorOf(Props.create(ConfigurationStorageActor.class));
+
+        
     }
 }
