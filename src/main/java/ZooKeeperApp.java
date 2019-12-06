50201cb188a8d3a764fbaf16ad9b25a973a6253f
Mamedov-V-E <black_snake666@mail.ru>
2019-12-07 01:54:57 +0300

    Scripted auto-commit on change (2019-12-07 01:54:57) by gitwatch.sh

--- src/main/java/ZooKeeperApp.java
+++ src/main/java/ZooKeeperApp.java
@@ -46,7 +46,7 @@ public class ZooKeeperApp {
                 3000,
                 event -> {
                     if (event.getType() == Watcher.Event.EventType.NodeCreated) {
-                            
+                        zoo.getChildren()
                     }
                 }
                 );
