a47dd5bdc9b4aedb9d71257262564a101cd4d777
Mamedov-V-E <black_snake666@mail.ru>
2019-12-07 02:14:48 +0300

    Scripted auto-commit on change (2019-12-07 02:14:48) by gitwatch.sh

--- src/main/java/StoreWatcher.java
+++ src/main/java/StoreWatcher.java
@@ -3,17 +3,18 @@ import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 
 public class StoreWatcher implements Watcher {
-    private final ZooKeeper zoo;
+    private final ZooKeeper zkClient;
 //    public synchronized void processWathcEvent(ZooKeeper zk, WatchedEvent event) {
 //
 //    }
     public StoreWatcher () throws  Exception{
-        zoo = new ZooKeeper(
+        zkClient = new ZooKeeper(
                 "sa" + ':' + 1233,
                 3000,
                 this
         );
     }
+
     public void process(WatchedEvent event) {
         if (event.getType() == Watcher.Event.EventType.NodeCreated) {
             .getChildren("/servers", this)
