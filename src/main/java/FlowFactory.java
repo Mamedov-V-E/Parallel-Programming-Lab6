0385df78ee80638aa6b2722e263c889c6ef8e60c
Mamedov-V-E <black_snake666@mail.ru>
2019-12-07 02:47:06 +0300

    Scripted auto-commit on change (2019-12-07 02:47:06) by gitwatch.sh

--- src/main/java/FlowFactory.java
+++ src/main/java/FlowFactory.java
@@ -8,11 +8,15 @@ import akka.stream.javadsl.Flow;
 
 
 public class FlowFactory {
+    private static final String SITE_PARAMETER_NAME = "url";
+    private static final String COUNT_PARAMETER_NAME = "count";
 
     public static Flow<HttpRequest, HttpResponse, NotUsed> createFlow(ActorSystem system, ActorMaterializer materializer) {
         return Flow.of(HttpRequest.class).map(r -> {
             Query q = r.getUri().query();
-            String site
+            String site = q.get(SITE_PARAMETER_NAME).get();
+            Integer count = Integer.parseInt(q.get(COUNT_PARAMETER_NAME).get());
+            
         })
     }
 }
