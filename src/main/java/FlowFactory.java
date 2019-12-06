47665e01b85139572779a491c253468d5f4d0853
Mamedov-V-E <black_snake666@mail.ru>
2019-12-07 02:45:03 +0300

    Scripted auto-commit on change (2019-12-07 02:45:03) by gitwatch.sh

--- src/main/java/FlowFactory.java
+++ src/main/java/FlowFactory.java
@@ -2,6 +2,7 @@ import akka.NotUsed;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.model.Query;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
@@ -10,7 +11,8 @@ public class FlowFactory {
 
     public static Flow<HttpRequest, HttpResponse, NotUsed> createFlow(ActorSystem system, ActorMaterializer materializer) {
         return Flow.of(HttpRequest.class).map(r -> {
-
+            Query q = r.getUri().query();
+            String site
         })
     }
 }
