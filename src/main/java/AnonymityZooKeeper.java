646b82d3eb32ba99c68606733a16e88bf611fc52
Mamedov-V-E <black_snake666@mail.ru>
2019-12-07 02:27:19 +0300

    Scripted auto-commit on change (2019-12-07 02:27:19) by gitwatch.sh

--- src/main/java/AnonymityZooKeeper.java
+++ src/main/java/AnonymityZooKeeper.java
@@ -24,7 +24,7 @@ public class AnonymityZooKeeper implements Watcher {
                 List<String> zkServers = zkClient.getChildren("/servers", this);
                 List<String> serversIdPort;
                 for (String s : zkServers) {
-                    byte[] port = zkClient.getData("")
+                    byte[] port = zkClient.getData("/servers/" + )
                 }
             } catch (Exception e) {
                 System.out.println(e.getMessage());
