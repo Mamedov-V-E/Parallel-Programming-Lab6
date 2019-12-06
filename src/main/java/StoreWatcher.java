706e7a150e3cf5315dd761c042e8c2944b5f2a5d
Mamedov-V-E <black_snake666@mail.ru>
2019-12-07 02:02:59 +0300

    Scripted auto-commit on change (2019-12-07 02:02:59) by gitwatch.sh

--- src/main/java/StoreWatcher.java
+++ src/main/java/StoreWatcher.java
@@ -1,7 +1,7 @@
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 
-public class StoreWatcher extends Watcher {
+public class StoreWatcher implements Watcher {
 
     public void process(WatchedEvent e) {
         
