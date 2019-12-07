bcfef297b2f735b781642a8c008e075bd22e06ba
Mamedov-V-E <black_snake666@mail.ru>
2019-12-07 03:21:33 +0300

    Scripted auto-commit on change (2019-12-07 03:21:32) by gitwatch.sh

--- src/main/java/FlowFactory.java
+++ src/main/java/FlowFactory.java
@@ -28,7 +28,7 @@ public class FlowFactory {
             final Http http = Http.get(system);
             if (count > 0) {
                 Future<Object> server = Patterns.ask(storeActor, new GetMessage(), 10000);
-                return http.singleRequest(HttpRequest.create(
+                return complete http.singleRequest(HttpRequest.create(
                         "http://" + server + "/?"
                                 + SITE_PARAMETER_NAME + "=" + site + "&"
                                 + COUNT_PARAMETER_NAME + "=" + (count-1))
