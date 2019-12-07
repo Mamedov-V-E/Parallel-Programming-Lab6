68f6fe10dee74f716332f40ec0a49f7b8fb93876
Mamedov-V-E <black_snake666@mail.ru>
2019-12-07 12:27:02 +0300

    Scripted auto-commit on change (2019-12-07 12:27:02) by gitwatch.sh

--- src/main/java/AnonymityZooKeeper.java
+++ src/main/java/AnonymityZooKeeper.java
@@ -34,7 +34,7 @@ public class AnonymityZooKeeper implements Watcher {
     }
 
     public void process(WatchedEvent event) {
-        System.out.println(event.getType());
+        System.out.println(event.getState());
         if (event.getType() == Event.EventType.NodeChildrenChanged) {
             try {
                 System.out.println("NODE CHANGED");
