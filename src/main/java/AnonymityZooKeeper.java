baf43554b302f0913cf0d9bf567e4a8a225444ca
Mamedov-V-E <black_snake666@mail.ru>
2019-12-07 12:21:24 +0300

    Scripted auto-commit on change (2019-12-07 12:21:24) by gitwatch.sh

--- src/main/java/AnonymityZooKeeper.java
+++ src/main/java/AnonymityZooKeeper.java
@@ -19,11 +19,11 @@ public class AnonymityZooKeeper implements Watcher {
                 3000,
                 this
         );
-        zkClient.delete("/servers", 0);
+//        zkClient.delete("/servers", 0);
         zkClient.create("/servers",
                 port.toString().getBytes(),
                 ZooDefs.Ids.OPEN_ACL_UNSAFE,
-                CreateMode.EPHEMERAL
+                CreateMode.PERSISTENT
         );
         zkClient.create("/servers/" + port,
                 port.toString().getBytes(),
